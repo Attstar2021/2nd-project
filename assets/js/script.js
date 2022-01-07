06c01597803e5215fb63cd08f9b37f245d6fc545
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-01-07 16:07:26 +0000

    Add correction to code through console

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,12 @@ const playerScore = document.getElementById("player-score");
 const resultsDisplay = document.getElementById("results");
 const computerImage = document.getElementById("computer-image");
 const playerImage = document.getElementById("player-image");
-const choices = ["rock", "paper", "scissors"]
+const choices = {
+    "rock":"/assets/images/Rock1.png",
+    "paper":"/assets/images/Rock1.png", 
+    "scissors":"/assets/images/Rock1.png"
+}
+let playerchoice = choices
 let buttons = document.getElementsByTagName("button");
 
 
@@ -17,32 +22,7 @@ let buttons = document.getElementsByTagName("button");
  */
 for (let button of buttons) {
     button.addEventListener("click", function(){
-        if(this,getAttribute("data-choice")==="0"){
-            displayImage.scr(assets/images/rock1.png)
-        } else if(this,getAttribute("data-choice")==="1"){
-            displayImage.scr(assets/images/paper.png)
-        } else if(this,getAttribute("data-choice")==="2"){
-            displayImage.scr(assets/images/Scissors1.png)
-        let playerChoice = this.getAttribute("data-choice");
-        playGame("playerChoice");
-    
+        if(this,getAttribute("choices"))
+        playGame(playerchoice);
+    });
 }
-
-
-
-/**Main game funtion*/
-
-function playGame (playerChoice){
-    playerImage.scr = `assets/images/${choice[playerChoice]}.png`;
-    playerImage.alt = choices [playerChoice]
-}
-
-
-let computerChoice = choices[Math.floor(Math.random()  * 3 + 1)]
-computerImage.scr = `assets/images/${choices[computerChoice]}.png`;
-computerImage.alt = choices[computerChoice]
-
-let results = resultsDisplay(choices[computerChoice],choices[playerChoice]);
-
-updatescores(results);
-
