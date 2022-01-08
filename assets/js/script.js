d2eab614007581849968b88c7bf3508318ae0804
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-01-08 10:47:42 +0000

    Add basic structure to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 /** This code has been taken from rock, paper and scissors game of code institue */
 /* Constants for DOM elements and choices
 */
-
 const computerScore = document.getElementById("computer-score");
 const playerScore = document.getElementById("player-score");
 const resultsDisplay = document.getElementById("results");
-const computerImage = document.getElementById("computer-image");
-const playerImage = document.getElementById("player-image");
-const choices = {
+const computerSelection = document.getElementById("computer-image");
+const playerSelection = document.getElementById("player-image");
+
+
+
+
+
+/*const choices = {
     "rock":"/assets/images/Rock1.png",
     "paper":"/assets/images/Rock1.png", 
     "scissors":"/assets/images/Rock1.png"
 }
-let playerchoice = choices
+const playerChoice = choices;
 let buttons = document.getElementsByTagName("button");
 
 
@@ -20,10 +24,17 @@ let buttons = document.getElementsByTagName("button");
 
 
 /**Add event listener to buttons
- */
+ *
+ 
+playerChoice.forEach(choices => choices,addEventListener('click', e=>
+{
+   playerChoice
+}))
 for (let button of buttons) {
     button.addEventListener("click", function(){
         if(this.getAttribute("data-choice") === "0")
         playerImage = `assets/images/Rock1.png`
     });
 }
+
+*/
